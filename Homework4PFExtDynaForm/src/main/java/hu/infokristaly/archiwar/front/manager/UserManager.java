b33904bde2ae851c92ea34021a5d6748a9f34ae2
Papp Zoltán <papp.zoltan@integrity.hu>
2016-07-09 14:41:41 +0200

    init

---
+++ Homework4PFExtDynaForm/src/main/java/hu/infokristaly/archiwar/front/manager/UserManager.java
@@ -0,0 +1,324 @@
+package hu.infokristaly.archiwar.front.manager;
+
+import hu.infokristaly.archiwar.back.domain.Clerk;
+import hu.infokristaly.archiwar.front.annotations.EntityInfo;
+import hu.infokristaly.archiwar.middle.services.ClerkService;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJBException;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.metadata.ConstraintDescriptor;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.primefaces.extensions.model.dynaform.DynaFormControl;
+import org.primefaces.extensions.model.dynaform.DynaFormLabel;
+import org.primefaces.extensions.model.dynaform.DynaFormModel;
+import org.primefaces.extensions.model.dynaform.DynaFormRow;
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
+
+import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Named
+@SessionScoped
+public class UserManager implements Serializable {
+
+	private static final long serialVersionUID = 3114254437517370726L;
+
+    @Inject
+    private Logger log;
+
+	@Inject
+	private ClerkService userService;
+
+	private List<ColumnModel> columns;
+	
+    private List<Clerk> selectedUsers;
+
+    private LazyDataModel<Clerk> lazyDataModel;
+    
+    private DynaFormModel formModel;
+
+    public UserManager() {
+    	
+    }
+
+    @PostConstruct
+    public void init() {
+        log.log(Level.INFO, "["+this.getClass().getName()+"] constructor finished.");
+        initModel();
+    }
+
+    private void initModel() {
+        columns = new ArrayList<ColumnModel>();
+        formModel = new DynaFormModel();
+        DynaFormRow row;// = formModel.createRegularRow();
+        //FieldModel model = new FieldModel("photo","hidden",false);        
+        DynaFormControl control;// = row.addControl(model,"hidden");
+        
+        Class<?> clazz = Clerk.class;
+        
+		Field[] fields = clazz.getDeclaredFields();
+		for (Field field : fields) {
+			if (field.isAnnotationPresent(EntityInfo.class)) {
+				EntityInfo entityInfo = field.getAnnotation(EntityInfo.class);
+				columns.add(new ColumnModel(entityInfo.info(),field.getName()));
+								
+				row = formModel.createRegularRow();
+				DynaFormLabel label = row.addLabel(entityInfo.info());
+				control = row.addControl(new FieldModel(field.getName(), entityInfo.info(), entityInfo.required()),entityInfo.editor());				
+				label.setForControl(control);
+			}
+		}
+	}
+
+	public LazyDataModel<Clerk> getLazyDataModel() {
+        if (lazyDataModel == null) {
+            lazyDataModel = new LazyDataModel<Clerk>() {
+
+                private static final long serialVersionUID = 1678907483750487431L;
+
+                private Map<String, Object> actualfilters;
+
+                private String actualOrderField;
+                private SortOrder actualSortOrder;
+                private Integer count;
+
+                @Override
+                public Clerk getRowData(String rowKey) {
+                    long primaryKey = Long.valueOf(rowKey);
+                    
+                    Clerk user = new Clerk(primaryKey);
+                    user = userService.find(user);
+                    return user;
+                }
+
+                @Override
+                public Object getRowKey(Clerk user) {
+                    return user.getId();
+                }
+
+                @Override
+                public List<Clerk> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
+                    this.setPageSize(pageSize);
+                    this.count = null;
+                    this.actualfilters = filters;
+                    if (sortField != null) {
+                        this.actualOrderField = sortField;
+                    }
+                    if (sortOrder != null) {
+                        this.actualSortOrder = sortOrder;
+                    }
+                    List<Clerk> result = (List<Clerk>) userService.findRange(first, pageSize, this.actualOrderField, this.actualSortOrder, filters);
+                    log.log(Level.INFO, "["+this.getClass().getName()+"] load finished.");
+                    return result;
+                }
+
+                @Override
+                public int getRowCount() {
+                	if (count == null) {
+                		count =  userService.count(actualfilters);
+                	}
+                    return count;
+                }
+
+            };
+        }
+        return lazyDataModel;
+    }
+
+	public List<Clerk> getSelectedUsers() {
+		return selectedUsers;
+	}
+
+	public void setSelectedUsers(List<Clerk> selectedUsers) {
+		this.selectedUsers = selectedUsers;
+	}
+
+    public List<ColumnModel> getColumns() {
+		return columns;
+	}
+
+	public void setColumns(List<ColumnModel> columns) {
+		this.columns = columns;
+	}
+
+	private Clerk value = new Clerk();
+
+	public Clerk getValue() {
+		return value;
+	}
+
+	public void setValue(Clerk user) {
+		this.value = user;
+	}
+
+	public DynaFormModel getFormModel() {
+		return formModel;
+	}
+
+	public void setFormModel(DynaFormModel formModel) {
+		this.formModel = formModel;
+	}
+
+	public void initValue() {
+		if (value.getId() != null) {
+			value = new Clerk();
+		}
+	}
+
+	public List<Clerk> getUserList() {
+		return userService.findAll(true);
+	}
+
+    public List<FieldModel> getProperties() {  
+        if (formModel == null) {  
+            return null;  
+        }  
+  
+        List<FieldModel> properties = new ArrayList<FieldModel>();  
+        for (DynaFormControl dynaFormControl : formModel.getControls()) {  
+            properties.add((FieldModel) dynaFormControl.getData());  
+        }  
+  
+        return properties;  
+    }
+    
+    public void clearProperties() {
+        for (DynaFormControl dynaFormControl : formModel.getControls()) {  
+            ((FieldModel)dynaFormControl.getData()).setValue(null);  
+        }        
+    }
+
+	public String save() {
+		List<FieldModel> fields = getProperties();
+		
+		fields.forEach(f -> {try {
+			BeanUtils.setProperty(value, f.propertyName, f.value);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}});		
+		try {
+			
+			if (value.getId() == null) {
+				userService.persist(value);
+			} else {
+				userService.merge(value);
+			}
+			
+			value = new Clerk();
+			clearProperties();
+            FacesMessage message = new FacesMessage("User saved.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+		} catch (EJBException e) {
+			Throwable ex = e.getCause();
+			if (ex instanceof ConstraintViolationException) {
+				Set<ConstraintViolation<?>> msg = ((ConstraintViolationException) ex).getConstraintViolations();
+				msg.forEach(c -> {
+					StringBuffer validationExcMsg = new StringBuffer();
+					ConstraintDescriptor<?> desc = c.getConstraintDescriptor();
+					System.out.println(desc.getAttributes().get("message"));
+					String temp = c.getMessageTemplate();
+					System.out.println(temp);
+					validationExcMsg.append(c.getPropertyPath()).append(":").append(c.getMessage());
+					FacesMessage message = new FacesMessage(validationExcMsg.toString());
+					FacesContext.getCurrentInstance().addMessage(null, message);
+				});
+			} else {
+				FacesMessage message = new FacesMessage("Failed: " + e.getMessage());
+				FacesContext.getCurrentInstance().addMessage(null, message);
+			}
+		}
+		return null;
+	}
+	
+	public void deleteSelected() {		
+		for(Clerk c: selectedUsers) {			
+			userService.removeSystemUser(c);
+		};
+	}
+
+    static public class ColumnModel implements Serializable {
+    	 
+		private static final long serialVersionUID = 4063465058747639784L;
+
+		private String header;
+        private String property;
+ 
+        public ColumnModel(String header, String property) {
+            this.header = header;
+            this.property = property;
+        }
+ 
+        public String getHeader() {
+            return header;
+        }
+ 
+        public String getProperty() {
+            return property;
+        }
+    }
+
+    static public class FieldModel implements Serializable {
+   	 
+		private static final long serialVersionUID = 4063465058747639784L;
+
+		private String label;
+		private String propertyName;
+        private Object value;
+        private boolean required;
+ 
+        public FieldModel(String propertyName,String label, boolean required) {
+        	this.propertyName = propertyName;
+            this.label = label;
+            this.required = required;
+        }
+
+		public String getLabel() {
+			return label;
+		}
+
+		public void setLabel(String label) {
+			this.label = label;
+		}
+
+		public String getPropertyName() {
+			return propertyName;
+		}
+
+		public void setPropertyName(String propertyName) {
+			this.propertyName = propertyName;
+		}
+
+		public Object getValue() {
+			return value;
+		}
+
+		public void setValue(Object value) {
+			this.value = value;
+		}
+
+		public boolean isRequired() {
+			return required;
+		}
+
+		public void setRequired(boolean required) {
+			this.required = required;
+		}
+ 
+    }
+
+}
---
+++ Homework4PFExtDynaForm/src/main/java/hu/infokristaly/archiwar/middle/services/ClerkService.java
@@ -0,0 +1,177 @@
+/**
+ * 
+ */
+package hu.infokristaly.archiwar.middle.services;
+
+import hu.infokristaly.archiwar.back.domain.Clerk;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import javax.ejb.Stateless;
+import javax.inject.Named;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.primefaces.model.SortOrder;
+
+/**
+ * @author pzoli
+ * 
+ */
+@Named
+@Stateless
+public class ClerkService extends BasicService<Clerk> implements Serializable {
+
+	private static final long serialVersionUID = -2360286862757860424L;
+
+	public Clerk find(Clerk systemUser) {
+		Clerk result = em.find(Clerk.class, systemUser.getId());
+		log.info("Find by Id [" + systemUser.getId() + "]"
+				+ systemUser.getOsUserName() + ":" + result);
+		return result;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Clerk> findRange(int first, int pageSize, String sortField,
+			SortOrder sortOrder, Map<String, Object> filters) {
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<Clerk> cq = builder.createQuery(Clerk.class);
+		Root<Clerk> from = cq.from(Clerk.class);
+		cq.select(from);
+
+		if ((filters != null) && !filters.isEmpty()) {
+			Set<Entry<String, Object>> es = filters.entrySet();
+			List<Predicate> predicateList = new ArrayList<Predicate>();
+			for (Entry<String, Object> filter : es) {
+				String field = filter.getKey();
+				Expression<String> x = from.get(field);
+				String pattern = filter.getValue() + "%";
+				x = builder.lower(x);
+				pattern = pattern.toLowerCase();
+				Predicate predicate = builder.like(x, pattern);
+				predicateList.add(predicate);
+			}
+
+			Predicate predicates = builder.and(predicateList
+					.toArray(new Predicate[predicateList.size()]));
+			cq.where(predicates);
+
+		}
+		if (sortField != null) {
+			if (sortOrder.equals(SortOrder.ASCENDING)) {
+				cq.orderBy(builder.asc(from.get(sortField)));
+			} else if (sortOrder.equals(SortOrder.DESCENDING)) {
+				cq.orderBy(builder.desc(from.get(sortField)));
+			}
+		}
+		Query q = em.createQuery(cq);
+		q.setMaxResults(pageSize);
+		q.setFirstResult(first);
+		return q.getResultList();
+	}
+
+	/**
+	 * Find All
+	 * 
+	 * @param first
+	 *            the first
+	 * @param pageSize
+	 *            the page size
+	 * @return the list
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Clerk> findAll(boolean active) {
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<Clerk> cq = builder.createQuery(Clerk.class);
+		Root<Clerk> from = cq.from(Clerk.class);
+		cq.select(from);
+		if (active) {
+			List<Predicate> predicateList = new ArrayList<Predicate>();
+
+			Expression<String> x = from.get("enabled");
+			Predicate predicate = builder.equal(x, true);
+			predicateList.add(predicate);
+
+			Predicate predicates = builder.and(predicateList
+					.toArray(new Predicate[predicateList.size()]));
+			cq.where(predicates);
+		}
+		cq.orderBy(builder.asc(from.get("userName")));
+		Query q = em.createQuery(cq);
+		return q.getResultList();
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public int count(Map<String, Object> actualFilters) {
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery cq = builder.createQuery();
+		Root<Clerk> from = cq.from(Clerk.class);
+		cq.select(builder.count(from));
+
+		if ((actualFilters != null) && !actualFilters.isEmpty()) {
+			Set<Entry<String, Object>> es = actualFilters.entrySet();
+			List<Predicate> predicateList = new ArrayList<Predicate>();
+			for (Entry<String, Object> filter : es) {
+				String field = filter.getKey();
+				Expression<String> x = from.get(field);
+				String pattern = filter.getValue() + "%";
+				x = builder.lower(x);
+				pattern = pattern.toLowerCase();
+				Predicate predicate = builder.like(x, pattern);
+				predicateList.add(predicate);
+			}
+
+			Predicate predicates = builder.and(predicateList
+					.toArray(new Predicate[predicateList.size()]));
+			cq.where(predicates);
+
+		}
+
+		Query q = em.createQuery(cq);
+		int result = ((Long) q.getSingleResult()).intValue();
+		return result;
+	}
+
+	public void removeSystemUser(Clerk item) {
+		item = find(item);
+		em.remove(item);
+	}
+
+	@Override
+	protected Query buildCountQuery(Map<String, Object> filters) {
+		String result = buildQueryString(filters);
+		result += "select count(*) " + result;
+		Query q = em.createQuery(result);
+		return q;
+	}
+
+	@Override
+	protected Query buildQuery(String sortField, SortOrder sortOrder,
+			Map<String, Object> filters) {
+		String result = buildQueryString(filters);
+		Query q = em.createQuery(result);
+		return q;
+	}
+
+	@Override
+	protected String buildQueryString(Map<String, Object> filters) {
+		StringBuffer queryBuff = new StringBuffer();
+		queryBuff.append("from Clerk");
+		return queryBuff.toString();
+	}
+
+	@Override
+	protected void setQueryParams(Query q, Map<String, Object> filters) {
+
+	}
+
+}
